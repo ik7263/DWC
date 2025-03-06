a4a554442119c732f210a791a8cd5b25bb4e379f
ik7263 <iankopf@hotmail.com>
2025-03-06 09:08:47 -0500

    updated deployment for pages

--- next.config.js
+++ next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export',
+  basePath: '/DWC',
+  assetPrefix: '/DWC/',
+  images: {
+    unoptimized: true,
+  },
 }
 
 module.exports = nextConfig 
